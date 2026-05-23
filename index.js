ca574d8f33979cb28788e3d6c5138a07904366ff
mahdihasanprogrammer <mahdihasan.code@gmail.com>
2026-05-23 19:54:21 +0600

    feat: add room booking API with time conflict validation

--- index.js
+++ index.js
@@ -28,36 +28,36 @@ const client = new MongoClient(uri, {
 
 // middleware;
 
-const verifyToken = async (req, res, next)=>{
+const verifyToken = async (req, res, next) => {
   const authHeader = req?.headers.authorization;
 
-  if(!authHeader){
-    return res.status(401).json({message:'Unauthorized'})
+  if (!authHeader) {
+    return res.status(401).json({ message: 'Unauthorized' })
   }
 
   const token = authHeader.split(' ')[1];
-  console.log(token, 'token')
-  if(!token){
-    return res.status(401).json({message:'Unauthorized'})
+
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' })
   }
 
-  
-  try{
-      const JWKS = createRemoteJWKSet(
-        new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
-      )
 
-      const {payload} = await jwtVerify(token, JWKS);
-      req.user= payload;
-      next()
-     
+  try {
+    const JWKS = createRemoteJWKSet(
+      new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
+    )
+
+    const { payload } = await jwtVerify(token, JWKS);
+    req.user = payload;
+    next()
+
   }
 
-  catch(error){
+  catch (error) {
     console.error('Token validation failed:', error)
     throw error
   }
-  
+
 }
 
 async function run() {
@@ -67,91 +67,131 @@ async function run() {
 
     const database = client.db('StudyNook');
     const roomsCollection = database.collection('rooms');
+    const bookingsCollection = database.collection('bookings');
 
 
     // get AvailableStudyRoom;
-    app.get('/available-rooms', async(req,res) =>{
+    app.get('/available-rooms', async (req, res) => {
       const availableRooms = await roomsCollection.find().limit(6).toArray();
       res.send(availableRooms)
     })
 
 
     // get all-rooms data;
-    app.get('/all-rooms', async(req, res) =>{
+    app.get('/all-rooms', async (req, res) => {
       const allRooms = await roomsCollection.find().toArray();
       res.send(allRooms)
     })
 
 
     // add room, api;
-    app.post('/add-room', async(req, res) =>{
+    app.post('/add-room', verifyToken, async (req, res) => {
       const roomData = req.body;
+     
+
+      if (roomData.userId !== req.user.id) {
+        return res.status(401).json({message:"Unauthorized"})
+      }
       const insertRoomData = await roomsCollection.insertOne(roomData);
       res.send(insertRoomData)
     })
 
 
     // get my-listings data throw get api ;
-    app.get('/my-listings/:userId', async(req, res) =>{
-      const {userId} = req.params;
-      const queryByUserId = {userId};
-      const myListingData = await roomsCollection.find(queryByUserId).toArray();
-      res.send(myListingData);
-  
+    app.get('/my-listings/:userId', verifyToken, async (req, res) => {
+      const { userId } = req.params;
+
+      if (userId !== req.user.id) {
+        return res.status(401).json({message:"Unauthorized"})
+      }
+      const myListingData = await roomsCollection.find({ userId }).toArray();
+      res.json(myListingData);
+      
+
     })
 
 
     // get single room details ;
-    app.get('/all-rooms/:id', async(req, res) =>{
-      const {id} = req.params;
-      const roomDetails = await roomsCollection.findOne({_id:new ObjectId(id)});
+    app.get('/all-rooms/:id', async (req, res) => {
+      const { id } = req.params;
+      const roomDetails = await roomsCollection.findOne({ _id: new ObjectId(id) });
       res.send(roomDetails)
     })
 
 
     // delete room by id;
-    app.delete('/all-rooms/:id',verifyToken, async(req, res) =>{
+    app.delete('/all-rooms/:id', verifyToken, async (req, res) => {
 
-      const {id} = req.params;
-      
-      const query = {_id: new ObjectId(id)};
+      const { id } = req.params;
+
+      const query = { _id: new ObjectId(id) };
       const room = await roomsCollection.findOne(query);
 
-      if(!room){
-         return res.status(404).send({ message: "Room not found" });
+      if (!room) {
+        return res.status(404).send({ message: "Room not found" });
       }
 
-      if(room.userEmail != req.user.email){
+      if (room.userEmail != req.user.email) {
         return res.status(403).send({ message: "Forbidden" });
       }
-      
+
       const deleteData = await roomsCollection.deleteOne(query);
       res.send(deleteData);
-    
+
     })
 
 
-      // patch data;
-        app.patch('/all-rooms/:id',verifyToken, async (req, res) => {
-            const id = req.params.id;
-            const modifiedRoom = req.body;
-          
+    // patch data;
+    app.patch('/all-rooms/:id', verifyToken, async (req, res) => {
+      const id = req.params.id;
+      const modifiedRoom = req.body;
 
-             const query = { _id: new ObjectId(id)};
-             const room = await roomsCollection.findOne(query);
-             
+      const query = { _id: new ObjectId(id) };
+      const room = await roomsCollection.findOne(query);
 
-             if(!room){
-                return res.status(404).send({message:'data not found'})
-             }
-             if(room.userEmail !== req.user.email){
-              return res.status(401).json({message:'Unauthorized'})
-             }
+      if (!room) {
+        return res.status(404).send({ message: 'data not found' })
+      }
+      if (room.userEmail !== req.user.email) {
+        return res.status(401).json({ message: 'Unauthorized' })
+      }
 
-            const updateData = await roomsCollection.updateOne(query, { $set: modifiedRoom });
-            res.send(updateData);
-        })
+      const updateData = await roomsCollection.updateOne(query, { $set: modifiedRoom });
+      res.send(updateData);
+    })
 
+
+    // Booking room;
+    app.post('/booking-room', verifyToken, async(req, res) =>{
+      const bookingRoom = req.body;
+      console.log(bookingRoom, 'backend')
+      
+      const conflictBooking = await bookingsCollection.findOne({
+        roomId: bookingRoom.roomId,
+        date: bookingRoom.date,
+        startTime :{
+          $lt: bookingRoom.endTime
+        },
+
+        endTime:{
+          $gt:bookingRoom.startTime
+        }
+      })
+
+      if(conflictBooking){
+      return  res.status(409).send({message:"Time slot already booked!"})
+      }
+
+      const bookingData = await bookingsCollection.insertOne(bookingRoom);
+      res.send({response:'ok', message:"booking room successful"})
+      
+      // if(bookingRoom.userId != req.user.id){
+      //   return res.status(401).json({message:"Unauthorized"})
+      // }
+
+
+
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("StudyNook").command({ ping: 1 });
